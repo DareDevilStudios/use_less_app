61c587dba095f95b57123a631308b3a59413c34c
Thrisha1 <thrishakannan25@gmail.com>
2024-10-25 17:36:15 +0530

    lamding page created

---
+++ src/components/common/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div>
+            <nav class="bg-white border-gray-200 dark:bg-gray-900">
+                <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                    <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+                        <h1 className="text-2xl">💀</h1>
+                        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Thantha vibe.ai</span>
+                    </a>
+                </div>
+            </nav>
+
+        </div>
+    )
+}
